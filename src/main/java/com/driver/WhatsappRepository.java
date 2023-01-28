41c84034f889555ebb8bae627c39d4fbd52cc11d
Bharath kumar K <2021bharathkumar@gmail.com>
2023-01-28 16:06:14 +0530

    SOLUTION!

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -13,6 +13,7 @@ public class WhatsappRepository {
     private HashMap<Group, List<Message>> groupMessageMap;
     private HashMap<Message, User> senderMap;
     private HashMap<Group, User> adminMap;
+    private HashMap<String,User> userHashMap=new HashMap<>();
     private HashSet<String> userMobile;
     private int customGroupCount;
     private int messageId;
@@ -26,4 +27,116 @@ public class WhatsappRepository {
         this.customGroupCount = 0;
         this.messageId = 0;
     }
+
+
+    public String createUser(String name, String mobile) throws Exception {
+        //If the mobile number exists in database, throw "User already exists" exception
+        //Otherwise, create the user and return "SUCCESS"
+         if(userHashMap.containsKey(name)){
+            throw new Exception( "User already exists");
+         }
+         else{
+             User user=new User(name,mobile);
+             userHashMap.put(name,user);
+             return "SUCCESS";
+         }
+
+    }
+
+    public  boolean isNewUser(String mobile){
+        if(userHashMap.containsKey(mobile)) return false;
+        return true;
+    }
+    public Group createGroup(List<User> users) {
+        // The list contains at least 2 users where the first user is the admin. A group has exactly one admin.
+        // If there are only 2 users, the group is a personal chat and the group name should be kept as the name of the second user(other than admin)
+        // If there are 2+ users, the name of group should be "Group count". For example, the name of first group would be "Group 1", second would be "Group 2" and so on.
+        // Note that a personal chat is not considered a group and the count is not updated for personal chats.
+        // If group is successfully created, return group.
+
+        //For example: Consider userList1 = {Alex, Bob, Charlie}, userList2 = {Dan, Evan}, userList3 = {Felix, Graham, Hugh}.
+        //If createGroup is called for these userLists in the same order, their group names would be "Group 1", "Evan", and "Group 2" respectively.
+
+         if(users.size()==2)
+            return this.createPersonalChat(users);
+
+             customGroupCount++;
+             Group group=new Group("Group"+customGroupCount,users.size());
+             groupUserMap.put(group,users);
+
+           adminMap.put(group,users.get(0));
+           return group;
+    }
+
+    public Group createPersonalChat(List<User> users) {
+        String groupName = users.get(1).getName();
+        Group personalGroup = new Group(groupName, 2);
+        groupUserMap.put(personalGroup, users);
+        return personalGroup;
+    }
+    public int createMessage(String content) {
+        // The 'i^th' created message has message id 'i'.
+        // Return the message id.
+        messageId++;
+        Date d=new Date();
+        Message message=new Message(messageId,content,d);
+        return messageId;
+    }
+
+    public int sendMessage(Message message, User sender, Group group) throws Exception {
+        //Throw "Group does not exist" if the mentioned group does not exist
+        //Throw "You are not allowed to send message" if the sender is not a member of the group
+        //If the message is sent successfully, return the final number of messages in that group.
+        if (!groupUserMap.containsKey(group)) throw new Exception("Group does not exist");
+        if (!this.userExistsInGroup(group, sender)) throw new Exception("You are not allowed to send message");
+
+        List<Message> messages = new ArrayList<>();
+        if(groupMessageMap.containsKey(group)) {
+            messages = groupMessageMap.get(group);
+        }
+        messages.add(message);
+        groupMessageMap.put(group, messages);
+        return messages.size();
+    }
+
+    public boolean userExistsInGroup(Group group, User sender) {
+        List<User> users = groupUserMap.get(group);
+        for(User user: users) {
+            if(user.equals(sender)) return true;
+        }
+
+        return false;
+    }
+    public String changeAdmin(User approver, User user, Group group) throws Exception {
+        //Throw "Group does not exist" if the mentioned group does not exist
+        //Throw "Approver does not have rights" if the approver is not the current admin of the group
+        //Throw "User is not a participant" if the user is not a part of the group
+        //Change the admin of the group to "user" and return "SUCCESS". Note that at one time there is only one admin and the admin rights are transferred from approver to user.
+
+//           if(!groupUserMap.containsKey(group)) throw new Exception("Group does not exist");
+//            if(!adminMap.get(group).equals(approver)) throw new Exception("Approver does not have rights");
+//            if(!this.userExistsInGroup(group, user)) throw  new Exception("User is not a participant");
+
+            adminMap.put(group, user);
+            return "SUCCESS";
+    }
+
+//    public int removeUser(User user) throws Exception {
+//        //This is a bonus problem and does not contains any marks
+//        //A user belongs to exactly one group
+//        //If user is not found in any group, throw "User not found" exception
+//        //If user is found in a group and it is the admin, throw "Cannot remove admin" exception
+//        //If user is not the admin, remove the user from the group, remove all its messages from all the databases, and update relevant attributes accordingly.
+//        //If user is removed successfully, return (the updated number of users in the group + the updated number of messages in group + the updated number of overall messages)
+//
+//        return whatsappRepository.removeUser(user);
+//    }
+//
+//    public String findMessage(Date start, Date end, int K) throws Exception {
+//        //This is a bonus problem and does not contains any marks
+//        // Find the Kth latest message between start and end (excluding start and end)
+//        // If the number of messages between given time is less than K, throw "K is greater than the number of messages" exception
+//
+//        return whatsappRepository.findMessage(start, end, K);
+//    }
 }
