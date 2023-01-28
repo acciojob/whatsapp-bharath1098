c466ff01e60960630791029fd2bd56c82b4ba595
Bharath kumar K <2021bharathkumar@gmail.com>
2023-01-28 17:24:11 +0530

    optimized SOLUTION4

--- src/main/java/com/driver/WhatsappRepository.java
+++ src/main/java/com/driver/WhatsappRepository.java
@@ -13,54 +13,40 @@ public class WhatsappRepository {
     private HashMap<Group, List<Message>> groupMessageMap;
     private HashMap<Message, User> senderMap;
     private HashMap<Group, User> adminMap;
-    private HashMap<String, User> userHashMap;
-    private HashSet<String> userMobile;
+    private HashMap<String, User> userData;
     private int customGroupCount;
     private int messageId;
 
-    public WhatsappRepository() {
+    public WhatsappRepository(){
         this.groupMessageMap = new HashMap<Group, List<Message>>();
         this.groupUserMap = new HashMap<Group, List<User>>();
         this.senderMap = new HashMap<Message, User>();
         this.adminMap = new HashMap<Group, User>();
-        this.userMobile = new HashSet<>();
-        this.userHashMap = new HashMap<>();
+        this.userData = new HashMap<>();
         this.customGroupCount = 0;
         this.messageId = 0;
     }
 
     public boolean isNewUser(String mobile) {
-        if (userHashMap.containsKey(mobile)) return false;
+        if(userData.containsKey(mobile)) return false;
         return true;
     }
+
     public String createUser(String name, String mobile) throws Exception {
         if(!this.isNewUser(mobile)) {
             throw new Exception("User already exists");
         }
-        userHashMap.put(mobile, new User(name, mobile));
+        userData.put(mobile, new User(name, mobile));
         return "SUCCESS";
     }
 
-
-
     public Group createGroup(List<User> users) {
-        // The list contains at least 2 users where the first user is the admin. A group has exactly one admin.
-        // If there are only 2 users, the group is a personal chat and the group name should be kept as the name of the second user(other than admin)
-        // If there are 2+ users, the name of group should be "Group count". For example, the name of first group would be "Group 1", second would be "Group 2" and so on.
-        // Note that a personal chat is not considered a group and the count is not updated for personal chats.
-        // If group is successfully created, return group.
-
-        //For example: Consider userList1 = {Alex, Bob, Charlie}, userList2 = {Dan, Evan}, userList3 = {Felix, Graham, Hugh}.
-        //If createGroup is called for these userLists in the same order, their group names would be "Group 1", "Evan", and "Group 2" respectively.
-
-        if (users.size() == 2)
-            return this.createPersonalChat(users);
+        if(users.size() == 2) return this.createPersonalChat(users);
 
         this.customGroupCount++;
-        String groupName="Group" + this.customGroupCount;
+        String groupName = "Group " + this.customGroupCount;
         Group group = new Group(groupName, users.size());
         groupUserMap.put(group, users);
-
         adminMap.put(group, users.get(0));
         return group;
     }
@@ -72,48 +58,43 @@ public class WhatsappRepository {
         return personalGroup;
     }
 
-    public int createMessage(String content) {
-        // The 'i^th' created message has message id 'i'.
-        // Return the message id.
+    public int createMessage(String content){
         this.messageId++;
-        Message message = new Message(messageId, content,new Date());
+        Message message = new Message(messageId, content, new Date());
         return this.messageId;
     }
 
-    public int sendMessage(Message message, User sender, Group group) throws Exception {
-        //Throw "Group does not exist" if the mentioned group does not exist
-        //Throw "You are not allowed to send message" if the sender is not a member of the group
-        //If the message is sent successfully, return the final number of messages in that group.
-        if (!groupUserMap.containsKey(group)) throw new Exception("Group does not exist");
-        if (!this.userExistsInGroup(group, sender)) throw new Exception("You are not allowed to send message");
+    public int sendMessage(Message message, User sender, Group group) throws Exception{
+        if(!groupUserMap.containsKey(group)) throw new Exception("Group does not exist");
+        if(!this.userExistsInGroup(group, sender)) throw  new Exception("You are not allowed to send message");
 
         List<Message> messages = new ArrayList<>();
-        if (groupMessageMap.containsKey(group)) messages = groupMessageMap.get(group);
+        if(groupMessageMap.containsKey(group)) messages = groupMessageMap.get(group);
 
         messages.add(message);
         groupMessageMap.put(group, messages);
-        senderMap.put(message,sender);
+        senderMap.put(message, sender);
         return messages.size();
     }
 
     public boolean userExistsInGroup(Group group, User sender) {
         List<User> users = groupUserMap.get(group);
-        for (User user : users) {
-            if (user.equals(sender)) return true;
+        for(User user: users) {
+            if(user.equals(sender)) return true;
         }
 
         return false;
     }
 
-    public String changeAdmin(User approver, User user, Group group) throws Exception {
-
-        if (!groupUserMap.containsKey(group)) throw new Exception("Group does not exist");
-        if (!adminMap.get(group).equals(approver)) throw new Exception("Approver does not have rights");
-        if (!this.userExistsInGroup(group, user)) throw new Exception("User is not a participant");
+    public String changeAdmin(User approver, User user, Group group) throws Exception{
+        if(!groupUserMap.containsKey(group)) throw new Exception("Group does not exist");
+        if(!adminMap.get(group).equals(approver)) throw new Exception("Approver does not have rights");
+        if(!this.userExistsInGroup(group, user)) throw  new Exception("User is not a participant");
 
         adminMap.put(group, user);
         return "SUCCESS";
     }
+
     public int removeUser(User user) throws Exception {
         Group userRelatedGroup = null;
         for(Group group: groupUserMap.keySet()) {
